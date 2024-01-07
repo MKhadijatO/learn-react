f8230864ff5ce34a3f48de2958edea07d3f942df
Khadijat <muhammadkhadijato@gmail.com>
2024-01-08 00:07:03 +0100

    react V18 method

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 // ReactDOM.render(<p>Hello World</p>, document.getElementById("sub"));
 
-ReactDOM.render(<div>
- <h1>React Course</h1>
+// ReactDOM.render(
+//   <div>
+//     <h1>React Course</h1>
+//     <p>We will learn react in this course.</p>
+//   </div>,
+//   document.getElementById("task")
+// );
+
+
+const task = ReactDOM.createRoot(document.getElementById("task"));
+
+task.render(
+    <div>
+    <h1>React Course</h1>
     <p>We will learn react in this course.</p>
-</div>, document.getElementById("task"));
+  </div>
+)

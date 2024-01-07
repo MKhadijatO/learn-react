c8927227c81c8b8d0221ebbe8706dc6221a2fcb0
Khadijat <muhammadkhadijato@gmail.com>
2024-01-07 23:55:53 +0100

    using

---
+++ index.js
@@ -0,0 +1,6 @@
+// ReactDOM.render(<p>Hello World</p>, document.getElementById("sub"));
+
+ReactDOM.render(<div>
+ <h1>React Course</h1>
+    <p>We will learn react in this course.</p>
+</div>, document.getElementById("task"));
